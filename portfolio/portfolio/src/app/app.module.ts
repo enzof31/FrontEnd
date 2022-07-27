ea18ee9ba143b88a03570a081859d3c3ac069658
enzof31 <enzoferreyra_31@hotmail.com>
2022-07-26 23:31:17 -0300

    Agrego:Acerca de,Experiencia,Educacion,Habilidades

--- portfolio/portfolio/src/app/app.module.ts
+++ portfolio/portfolio/src/app/app.module.ts
@@ -8,6 +8,11 @@ import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
 import { RedSocialesImgComponent } from './components/red-sociales-img/red-sociales-img.component';
 import { BackgroundPageComponent } from './components/background-page/background-page.component';
 import { BanerComponent } from './components/baner/baner.component';
+import { AcercaComponent } from './components/acerca/acerca.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
+import { HabilidadesComponent } from './components/habilidades/habilidades.component';
+import { NgCircleProgressModule } from 'ng-circle-progress';
 
 @NgModule({
   declarations: [
@@ -16,11 +21,16 @@ import { BanerComponent } from './components/baner/baner.component';
     LogoAPComponent,
     RedSocialesImgComponent,
     BackgroundPageComponent,
-    BanerComponent
+    BanerComponent,
+    AcercaComponent,
+    ExperienciaComponent,
+    EducacionComponent,
+    HabilidadesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgCircleProgressModule.forRoot({})
   ],
   providers: [],
   bootstrap: [AppComponent]
